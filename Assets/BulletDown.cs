87f7beb334c93ce7e9cd3782820cafd95ec956e9
MartinKulevski <mkulevski1@myjmc.edu.au>
2023-08-14 15:59:13 +1000

    ALOT OF WORKKK

--- Assets/BulletDown.cs
+++ Assets/BulletDown.cs
@@ -10,6 +10,10 @@ public class BulletDown : MonoBehaviour
     public GameObject gameOverScreen;
     public GameObject BulletRefill;
 
+    public Image spriteRenderer;
+    public Sprite[] ammoUI;
+    public int currentAmmoUI;
+
     private void Start()
     {
         ammoText.text = "Ammo: " + ammoCount;
@@ -32,7 +36,7 @@ public class BulletDown : MonoBehaviour
     }
 
     IEnumerator WaitForGameOverScreen()
-    {
+    { 
         yield return new WaitForSeconds(1.5f);
         gameOverScreen.SetActive(true);
     }
@@ -49,7 +53,31 @@ public class BulletDown : MonoBehaviour
 
     private void UpdateAmmoText()
     {
-        ammoText.text = "Ammo: " + ammoCount.ToString();
+        //ammoText.text = "Ammo: " + ammoCount.ToString();
+        if(ammoCount == 5)
+        {
+            spriteRenderer.sprite = ammoUI[0];
+        }
+        if (ammoCount == 4)
+        {
+            spriteRenderer.sprite = ammoUI[1];
+        }
+        if (ammoCount == 3)
+        {
+            spriteRenderer.sprite = ammoUI[2];
+        }
+        if (ammoCount == 2)
+        {
+            spriteRenderer.sprite = ammoUI[3];
+        }
+        if (ammoCount == 1)
+        {
+            spriteRenderer.sprite = ammoUI[4];
+        }
+        if (ammoCount == 0)
+        {
+            spriteRenderer.sprite = ammoUI[5];
+        }
     }
 
     
